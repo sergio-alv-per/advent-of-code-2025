cea33fa386b1b326cb16a5065fe57c25c9ebc191
sergio-alv-per <sergioalvarezperez00@gmail.com>
2025-12-05 23:52:12 +0100

    day 04

---
+++ src/bin/04_1.rs
@@ -0,0 +1,99 @@
+use aoc_2025::parse_input;
+use std::io::{self, Read};
+use winnow::Parser;
+use winnow::Result;
+use winnow::ascii::newline;
+use winnow::combinator::{alt, fail, repeat, separated};
+
+enum Square {
+    PaperRoll,
+    Empty,
+}
+
+fn parse_square(input: &mut &str) -> Result<Square> {
+    alt((
+        '.'.map(|_| Square::Empty),
+        '@'.map(|_| Square::PaperRoll),
+        fail,
+    ))
+    .parse_next(input)
+}
+
+fn parse_row(input: &mut &str) -> Result<Vec<Square>> {
+    repeat(1.., parse_square).parse_next(input)
+}
+
+fn parse_grid(input: &mut &str) -> Result<Vec<Vec<Square>>> {
+    separated(1.., parse_row, newline).parse_next(input)
+}
+
+fn roll_in_adjacent(
+    i: usize,
+    j: usize,
+    i_dir: isize,
+    j_dir: isize,
+    grid: &Vec<Vec<Square>>,
+) -> bool {
+    let rows = grid.len();
+    let cols = grid.first().expect("grid didn't have any rows").len();
+    let possible_adj_i = i.checked_add_signed(i_dir).filter(|&adj_i| adj_i < rows);
+    let possible_adj_j = j.checked_add_signed(j_dir).filter(|&adj_j| adj_j < cols);
+
+    return match (possible_adj_i, possible_adj_j) {
+        (Some(adj_i), Some(adj_j)) => match grid[adj_i][adj_j] {
+            Square::Empty => false,
+            Square::PaperRoll => true,
+        },
+        _ => false,
+    };
+}
+
+fn rolls_in_adjacent_squares(i: usize, j: usize, grid: &Vec<Vec<Square>>) -> i32 {
+    let mut rolls = 0;
+
+    for i_dir in -1..=1 {
+        for j_dir in -1..=1 {
+            if (i_dir != 0 || j_dir != 0) && roll_in_adjacent(i, j, i_dir, j_dir, grid) {
+                rolls += 1;
+            }
+        }
+    }
+
+    rolls
+}
+
+fn solve(input: &str) -> i32 {
+    let grid = parse_input!(parse_grid, input);
+
+    grid.iter()
+        .enumerate()
+        .flat_map(|(i, row)| row.iter().enumerate().map(move |(j, sq)| (i, j, sq)))
+        .filter_map(|(i, j, sq)| {
+            if let Square::PaperRoll = sq {
+                Some(rolls_in_adjacent_squares(i, j, &grid))
+            } else {
+                None
+            }
+        })
+        .filter(|&n_rolls| n_rolls < 4)
+        .count()
+        .try_into()
+        .unwrap()
+}
+
+fn main() {
+    let mut input = String::new();
+    io::stdin()
+        .read_to_string(&mut input)
+        .expect("error reading input from stdin");
+    let solution = solve(&input);
+    println!("{solution}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::solve;
+    use aoc_2025::test_with_files;
+
+    test_with_files!("04", solve, 13, 1480);
+}
