4eca3a3ca5d5ef88507ccedfa7e9697f6f60085d
sergio-alv-per <sergioalvarezperez00@gmail.com>
2025-12-20 00:38:49 +0100

    reimplement 10_1 as 10_2

--- src/bin/10_1.rs
+++ src/bin/10_1.rs
@@ -1,144 +1,128 @@
 use aoc_2025::parse_input;
 use std::io::{self, Read};
+use std::iter::Sum;
 use winnow::Parser;
 use winnow::Result;
 use winnow::ascii::{dec_uint, newline};
 use winnow::combinator::{alt, delimited, repeat, separated, seq, terminated};
-use z3::Solver;
-use z3::ast::BV;
-use z3::ast::Bool;
-
-const MAX_LIGHTMAP_SIZE: u32 = 16;
-const MAX_BUTTON_PRESSES: u64 = 100;
-
-#[derive(Clone, Copy)]
-struct LightDiagram {
-    activated_lights: u16,
-}
-
-impl From<Vec<bool>> for LightDiagram {
-    fn from(value: Vec<bool>) -> Self {
-        LightDiagram {
-            activated_lights: value.iter().enumerate().fold(0, |acc, (i, &light)| {
-                if light { acc | (1 << i) } else { acc }
-            }),
-        }
-    }
-}
-
-impl From<Vec<u16>> for LightDiagram {
-    fn from(value: Vec<u16>) -> Self {
-        LightDiagram {
-            activated_lights: value.iter().fold(0, |acc, i| acc | (1 << i)),
-        }
-    }
-}
-
-impl From<u16> for LightDiagram {
-    fn from(value: u16) -> Self {
-        LightDiagram {
-            activated_lights: value,
-        }
-    }
-}
-
-impl From<LightDiagram> for BV {
-    fn from(value: LightDiagram) -> Self {
-        BV::from_u64(value.activated_lights.into(), MAX_LIGHTMAP_SIZE)
-    }
-}
+use z3::Optimize;
+use z3::ast::Int;
 
 struct ProblemRow {
-    target_light_diagram: LightDiagram,
-    buttons: Vec<LightDiagram>,
-    _joltages: Vec<u16>,
+    light_map: Vec<bool>,
+    buttons: Vec<Vec<u16>>,
 }
 
 fn parse_light(input: &mut &str) -> Result<bool> {
     alt(('.', '#')).map(|c| c == '#').parse_next(input)
 }
-fn parse_light_diagram(input: &mut &str) -> Result<LightDiagram> {
-    delimited('[', repeat(1.., parse_light), ']')
-        .map(|lights: Vec<bool>| LightDiagram::from(lights))
-        .parse_next(input)
+fn parse_light_map(input: &mut &str) -> Result<Vec<bool>> {
+    delimited('[', repeat(1.., parse_light), ']').parse_next(input)
 }
 
-fn parse_u6_vec(input: &mut &str) -> Result<Vec<u16>> {
+fn parse_u16_vec(input: &mut &str) -> Result<Vec<u16>> {
     separated(1.., dec_uint::<_, u16, _>, ',').parse_next(input)
 }
-fn parse_button(input: &mut &str) -> Result<LightDiagram> {
-    delimited('(', parse_u6_vec, ')')
-        .map(LightDiagram::from)
-        .parse_next(input)
+fn parse_button(input: &mut &str) -> Result<Vec<u16>> {
+    delimited('(', parse_u16_vec, ')').parse_next(input)
 }
 
-fn parse_buttons(input: &mut &str) -> Result<Vec<LightDiagram>> {
+fn parse_buttons(input: &mut &str) -> Result<Vec<Vec<u16>>> {
     separated(1.., parse_button, ' ').parse_next(input)
 }
 
 fn parse_joltages(input: &mut &str) -> Result<Vec<u16>> {
-    delimited('{', parse_u6_vec, '}').parse_next(input)
+    delimited('{', parse_u16_vec, '}').parse_next(input)
 }
 
 fn parse_problem_row(input: &mut &str) -> Result<ProblemRow> {
-    seq! {ProblemRow{target_light_diagram: parse_light_diagram, _: ' ', buttons: parse_buttons, _: ' ', _joltages: parse_joltages}}.parse_next(input)
+    seq! {ProblemRow{light_map: parse_light_map, _: ' ', buttons: parse_buttons, _: ' ', _: parse_joltages}}.parse_next(input)
 }
 
 fn parse_problem(input: &mut &str) -> Result<Vec<ProblemRow>> {
     repeat(1.., terminated(parse_problem_row, newline)).parse_next(input)
 }
 
-fn solvable_in_n_button_presses(problem_row: &ProblemRow, n: u64) -> bool {
-    let solver = Solver::new();
-    let zero_bv = BV::from_u64(0, MAX_LIGHTMAP_SIZE);
-    let variables: Vec<BV> = {
-        let mut vars = Vec::with_capacity(n.try_into().expect("u64 does not fit in usize"));
+fn button_machine_activation_matrix(buttons: &[Vec<u16>], n_machines: usize) -> Vec<Vec<bool>> {
+    let mut button_machine_matrix: Vec<Vec<bool>> = vec![vec![false; n_machines]; buttons.len()];
 
-        for _ in 0..n {
-            vars.push(BV::fresh_const("press", MAX_LIGHTMAP_SIZE));
+    for (i, button) in buttons.iter().enumerate() {
+        for &j in button {
+            button_machine_matrix[i][j as usize] = true;
         }
-        vars
-    };
-
-    let target_bv = BV::from(problem_row.target_light_diagram);
-
-    solver.assert(
-        variables
-            .iter()
-            .fold(target_bv, |acc, e| acc.bvxor(e))
-            .eq(&zero_bv),
-    );
-
-    for pushed_button in &variables {
-        let equals_some_button = problem_row
-            .buttons
-            .iter()
-            .fold(Bool::from_bool(false), |acc, but| {
-                acc | pushed_button.eq(BV::from(*but))
-            });
-
-        solver.assert(equals_some_button | pushed_button.eq(&zero_bv));
     }
 
-    match solver.check() {
-        z3::SatResult::Unsat => false,
-        z3::SatResult::Sat => true,
-        z3::SatResult::Unknown => panic!("problem result unknown"),
-    }
+    button_machine_matrix
 }
 
-fn min_n_to_solve_problem(pr: &ProblemRow) -> u64 {
-    let button_presses_array: Vec<u64> = (0..MAX_BUTTON_PRESSES).collect();
-    button_presses_array
-        .partition_point(|&n| !solvable_in_n_button_presses(pr, n))
-        .try_into()
-        .expect("usize did not fit in u64")
+fn init_z3_variables(n_variables: usize, problem_i: usize) -> Vec<Int> {
+    let mut variables = Vec::with_capacity(n_variables);
+
+    for button_i in 0..n_variables {
+        variables.push(Int::new_const(format!("press_{problem_i}_{button_i}")));
+    }
+    variables
+}
+
+fn min_button_presses(pr: &ProblemRow, problem_index: usize) -> u64 {
+    let button_machine_matrix = button_machine_activation_matrix(&pr.buttons, pr.light_map.len());
+
+    let optimizer = Optimize::new();
+    let button_presses = init_z3_variables(button_machine_matrix.len(), problem_index);
+
+    let total_button_presses = Int::sum(button_presses.iter());
+
+    optimizer.minimize(&total_button_presses);
+
+    for bp in &button_presses {
+        let bp_geq_zero = &bp.ge(0);
+        optimizer.assert(bp_geq_zero);
+    }
+
+    for (j, &light_activated) in pr.light_map.iter().enumerate() {
+        let button_presses_affecting_this_machine: Vec<&Int> = (0..button_machine_matrix.len())
+            .filter_map(|i| {
+                if button_machine_matrix[i][j] {
+                    Some(&button_presses[i])
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        let sum_button_presses_equals_light_activated =
+            Int::sum(button_presses_affecting_this_machine.into_iter())
+                .modulo(2)
+                .eq(if light_activated { 1 } else { 0 });
+
+        optimizer.assert(&sum_button_presses_equals_light_activated);
+    }
+
+    match optimizer.check(&[]) {
+        z3::SatResult::Sat => {
+            if let Some(model) = optimizer.get_model()
+                && let Some(presses) = model.eval(&total_button_presses, true)
+                && let Some(presses) = presses.as_u64()
+            {
+                presses
+            } else {
+                panic!("solution extraction went wrong")
+            }
+        }
+        z3::SatResult::Unsat | z3::SatResult::Unknown => {
+            panic!("problem not solvable")
+        }
+    }
 }
 
 fn solve(input: &str) -> u64 {
     let problems = parse_input!(parse_problem, input);
 
-    problems.iter().map(min_n_to_solve_problem).sum()
+    problems
+        .iter()
+        .enumerate()
+        .map(|(i, pr)| min_button_presses(pr, i))
+        .sum()
 }
 
 fn main() {
